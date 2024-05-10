88eac0de8158a987c7ba1abe9d692ccff6630706
Albert <albert@rss3.io>
2024-05-10 10:28:11 +0800

    refactor

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 This is a containerd solution to run a VSL RPC Node.
 
 ## Configuration
-replace OP_NODE_L1_ETH_RPC to "your-ethereum-mainnet-rpc" in op-node.env
+replace OP_NODE_L1_ETH_RPC to "your-ethereum-mainnet-rpc" in .env
 
 
 For the initial deployment, generate the secret files:
--- gen-secret.sh
+++ gen-secret.sh
@@ -2,7 +2,9 @@
 set -x
 
 echo "gen opnode_p2p_priv.txt"
-openssl rand -hex 32 > ./config/opnode_p2p_priv.txt
+openssl rand -hex 32 > ./mainnet/opnode_p2p_priv.txt
+openssl rand -hex 32 > ./sepolia/opnode_p2p_priv.txt
 
 echo "gen jwt-secret.txt"
-openssl rand -hex 32 > ./config/jwt-secret.txt
+openssl rand -hex 32 > ./mainnet/jwt-secret.txt
+openssl rand -hex 32 > ./sepolia/jwt-secret.txt
